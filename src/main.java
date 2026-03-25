748fbea157c33e7e18e28b63263aa4533eed7b67
Matthew Romano <matbat2205@gmail.com>
2026-03-25 14:13:28 -0400

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,1 +1,11 @@
-// Hello Matt
+package src;
+import src.core.Tournament;
+import src.factories.TournamentFactory;
+
+// Matthew Romano - March
+
+public class main {
+    public static void main(String[] args) {
+        TournamentFactory factory = new TournamnetFactory();
+    }
+}
