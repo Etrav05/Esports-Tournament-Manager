1a8a29da1d9647314dd7bade284927b4a60202a8
Etrav05 <Etravis9851@conestogac.on.ca>
2026-03-25 16:30:01 -0400

    Moved generateBracket() to each tournamentformat so they now work dynamically with the enum

--- src/core/TournamentFormat.java
+++ src/core/TournamentFormat.java
@@ -1,10 +1,25 @@
 package src.core;
 
+import java.util.ArrayList;
+import src.models.Match;
+import src.models.Team;
+
 public enum TournamentFormat {
     SINGLE_ELIM {
         @Override
         public int calculateRounds(int teams) {
             return (int) (Math.log(teams) / Math.log(2));
+        }
+
+        @Override
+          public ArrayList<Match> generateBracket(ArrayList<Team> teams) {
+            ArrayList<Match> matches = new ArrayList<>();
+
+            for (int i = 0; i < teams.size() - 1; i += 2) {
+                matches.add(new Match(teams.get(i), teams.get(i + 1)));
+            }
+
+            return matches;
         }
     },
 
@@ -13,6 +28,18 @@ public enum TournamentFormat {
         public int calculateRounds(int teams) {
             return (int) (Math.log(teams) / Math.log(2)) * 2;
         }
+
+        @Override
+        public ArrayList<Match> generateBracket(ArrayList<Team> teams) {
+            ArrayList<Match> matches = new ArrayList<>();
+
+            for (int i = 0; i < teams.size() - 1; i += 2) {
+                matches.add(new Match(teams.get(i), teams.get(i + 1)));
+            }
+
+            return matches;
+        }
+
     },
     
     ROUND_ROBIN {
@@ -20,7 +47,21 @@ public enum TournamentFormat {
         public int calculateRounds(int teams) {
             return teams - 1;
         }
+
+        @Override
+        public ArrayList<Match> generateBracket(ArrayList<Team> teams) {
+            ArrayList<Match> matches = new ArrayList<>();
+
+            for (int i = 0; i < teams.size(); i++) {
+                for (int j = i + 1; j < teams.size(); j++) {
+                    matches.add(new Match(teams.get(i), teams.get(j)));
+                }
+            }
+
+            return matches;
+        }
     };
 
     public abstract int calculateRounds(int teams);
+    public abstract ArrayList<Match> generateBracket(ArrayList<Team> teams);
 }
